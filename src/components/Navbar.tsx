d9abd7d5fc2b00c721cf4d73bbf76cc42e458aee
Wilson Roserie <wilsonroserie1@gmail.com>
2025-11-22 16:07:44 +0000

    Restore wordmark in Navbar logo

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -28,7 +28,7 @@ function Navbar() {
         <div className="flex items-center justify-between">
           {/* Logo */}
           <Link to="/dashboard" className="hover:opacity-90" aria-label="Simple RubriQ Home">
-            <Logo className="h-8 sm:h-10 transition-transform hover:scale-105" hideWordmark />
+            <Logo className="h-8 sm:h-10 transition-transform hover:scale-105" />
           </Link>
 
           {/* Desktop Navigation */}
